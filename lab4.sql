a126e8a161e81a4939a7aa64ab4b6b8dae3a0866
Oscar <oscarmaldonadoj23@gmail.com>
2023-02-01 20:43:34 -0800

    tables created and values inserted, q1 done

--- lab4.sql
+++ lab4.sql
@@ -2,47 +2,50 @@
 -- allow you to map a subset of the data from the ‘alzheimers’ table into tables
 -- with the following schema:
 
--- create table questions (
--- 	id int,
--- 	question_id varchar(20),
--- 	question varchar(500),
--- 	primary key (id, question_id)
--- );
+create table questions (
+	id int,
+	question_id varchar(20),
+	question text,
+	primary key (id, question_id)
+);
 
-
--- insert into questions
 insert into questions(id, question_id, question) (
 	select id, question_id, question
 	from alzheimers 
 );
+
+create table responses (
+	id int,
+	question_id varchar(20) ,
+	data_value varchar(500),
+	data_value_unit varchar(20),
+	foreign key (id, question_id) references questions(id, question_id),
+	primary key (id, question_id)
+);
+
 insert into responses (id, question_id, data_value, data_value_unit) (
 	select id, question_id, data_value, data_value_unit 
 	from alzheimers
 );
+
+create table results (
+	id int,
+	loc_abbrev VARCHAR(20),
+	source VARCHAR(20),
+	class text,
+	topic text,
+	question_id varchar(20),
+	foreign key (id, question_id) references questions(id, question_id),
+	primary key(id, question_id)
+);
+
 insert into results(id, loc_abbrev, source, class, topic, question_id) (
 	select id, loc_abbrev, source, class, topic, question_id
 	from alzheimers
 );
--- create table responses (
--- 	id int,
--- 	question_id varchar(20) ,
--- 	data_value varchar(500),
--- 	data_value_unit varchar(20),
--- 	foreign key (id, question_id) references questions(id, question_id),
--- 	primary key (id, question_id)
--- );
 
-
--- create table results (
--- 	id int,
--- 	loc_abbrev VARCHAR(20),
--- 	source VARCHAR(20),
--- 	class VARCHAR(200),
--- 	topic VARCHAR(200),
--- 	question_id varchar(20),
--- 	foreign key (id, question_id) references questions(id, question_id),
--- 	primary key(id, question_id)
--- );
+select * from questions 
+limit 10;
 -- (b) For the results table, the primary key can be the id field, as it is guaranteed to be unique for each record. 
 -- The question_id is also included as a key as it references the questions table.
 
